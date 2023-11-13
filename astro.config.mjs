1acd1a6b7d048a728b2552b3de94c4798a3d08b4
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2023-11-13 16:58:26 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import remarkGfm from "remark-gfm";
+import rehypePrism from "@mapbox/rehype-prism";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kylewigs.com",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+    }),
+  ],
 });
