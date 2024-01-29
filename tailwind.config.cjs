4b9d9fd24df0a8d03086a7d1ac3d1c405a3d7437
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-01-28 22:29:12 -0500

    fix color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: ["media"],
   theme: {
     extend: {
       fontFamily: {
