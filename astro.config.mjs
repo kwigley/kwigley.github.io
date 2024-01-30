7e6e266170331f2efe18dbcc39e79aa3af4c7c04
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-01-29 23:52:35 -0500

    mess with shadui

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind({applyBaseStyles: false}), sitemap(), robotsTxt(), react()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
