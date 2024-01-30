989729c4e10371fe72c80fd1b093487d77f6fb46
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-01-29 23:54:13 -0500

    fix tailwind base styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
